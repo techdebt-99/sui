54c4cd37e9c27da0cf9c85c63b959c40b8a6d2de
Andrew Schran <aschran@mystenlabs.com>
2023-11-08 15:37:11 -0800

    Write randomness output from narwhal in Sui

--- crates/sui-core/src/authority/authority_per_epoch_store.rs
+++ crates/sui-core/src/authority/authority_per_epoch_store.rs
@@ -774,10 +774,9 @@ impl AuthorityPerEpochStore {
             .is_some()
     }
 
-    // Returns true if authenticator state is enabled in the protocol config *and* the
-    // authenticator state object already exists
+    // Returns true if randomness state is enabled in the protocol config *and* the
+    // randomness state object already exists
     pub fn randomness_state_enabled(&self) -> bool {
-        // TODO-DNS use this somewhere?
         self.protocol_config().random_beacon() && self.randomness_state_exists()
     }
 
@@ -1987,6 +1986,10 @@ impl AuthorityPerEpochStore {
                     return None;
                 }
             }
+            SequencedConsensusTransactionKind::External(ConsensusTransaction {
+                kind: ConsensusTransactionKind::RandomnessStateUpdate(_round, _bytes),
+                ..
+            }) => {} // TODO-DNS anything to verify?
             SequencedConsensusTransactionKind::System(_) => {}
         }
         Some(VerifiedSequencedConsensusTransaction(transaction))
@@ -2473,6 +2476,13 @@ impl AuthorityPerEpochStore {
                     );
                 }
                 Ok(ConsensusCertificateResult::ConsensusMessage)
+            }
+            SequencedConsensusTransactionKind::External(ConsensusTransaction {
+                kind: ConsensusTransactionKind::RandomnessStateUpdate(_, _),
+                ..
+            }) => {
+                // These are always generated as System transactions (handled below).
+                panic!("process_consensus_transaction called with external RandomnessStateUpdate");
             }
             SequencedConsensusTransactionKind::System(system_transaction) => {
                 if !self
